0f4c113896c4ee17d774b1e4178a7c9bf0e06038
vianadaniel <vianadaniel@hotmail.com>
2020-08-14 11:46:28 -0300

    card

--- whatslab/src/App.js
+++ whatslab/src/App.js
@@ -1,26 +1,117 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styled from 'styled-components'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Principal = styled.div`
+display: flex;
+flex-direction: row;
+margin-top: 10px;
+justify-content: center;
+
+`
+const EnviarMsg = styled.div`
+display: flex;
+flex-direction: row;
+position: relative;
+top: 40%;
+
+`
+const Botao = styled.button`
+
+`
+
+const SectionUser = styled.div`
+width: 150px;
+display: flex;
+    margin-bottom: 10px;
+    height: 80vh;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    border: 1px solid gray;
+`
+const SectionMsg = styled.div`
+width: 400px;
+display: flex;
+    margin-bottom: 10px;
+    height: 80vh;
+    flex-direction: column;
+    justify-content: space-evenly;
+    align-items: center;
+    border: 1px solid gray;
+`
+const InputMsg = styled.input`
+width: 250px;
+
+`
+class App extends React.Component {
+
+  state = {
+    
+    valorInputUsuario: "",
+    valorInputFoto: "",
+    valorInputFotoPost: "",
+
+  }
+
+  adicionaPost = () => {
+    
+    const novoPost = {
+     
+      nomeUsuario: this.state.valorInputUsuario,
+      
+      foto: this.state.valorInputFoto,
+      fotoPost: this.state.inputFotoPost
+    };
+
+    
+    const novoPosts = [...this.state.posts, novoPost];
+
+    
+    this.setState({ posts: novoPosts });
+    this.setState({ valorInputPessoa: "" });
+    console.log(novoPosts)
+  };
+
+  onChangeInputUsuario = (event) => {
+    
+    this.setState({ valorInputUsuario: event.target.value });
+  };
+
+  onChangeInputFoto = (event) => {
+   
+    this.setState({ valorInputFoto: event.target.value });
+  };
+  onChangeInputFotoPost = (event) => {
+  
+    this.setState({ inputFotoPost: event.target.value });
+  };
+
+  
+
+  render() {
+
+    
+    
+    
+
+    return (
+      <Principal>
+      <SectionUser>
+
+      </SectionUser>
+      <SectionMsg>
+        <EnviarMsg>
+        <InputMsg/>
+        <Botao>Enviar</Botao>
+        </EnviarMsg>
+        
+      </SectionMsg>
+      
+    </Principal>
+      
+    );
+  }
+
 }
 
 export default App;
